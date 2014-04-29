5dca9be4b22722dc44e32f4876d82751ee28d4a4
tranfuga25s <tranfuga25s@gmail.com>
2014-04-29 09:26:27 -0300

    Agregados or ignore que faltaban - fixes #40

--- test/sql/QSQLITE/compras_productos.sql
+++ test/sql/QSQLITE/compras_productos.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS compras_productos ( id INTEGER PRIMARY KEY AUTOINCREMENT, id_compra NUMERIC , id_producto NUMERIC  , precio_compra NUMERIC , cantidad NUMERIC  );
 INSERT OR IGNORE INTO "sqlite_sequence" ( "name", "seq" ) VALUES ( 'compras_productos', '1' );
-INSERT INTO compras_productos VALUES ( 1, 1, 1, 10.0, 1.0 );
-INSERT INTO compras_productos VALUES ( 2, 2, 2, 10.0, 2.0 );
+INSERT OR IGNORE INTO compras_productos VALUES ( 1, 1, 1, 10.0, 1.0 );
+INSERT OR IGNORE INTO compras_productos VALUES ( 2, 2, 2, 10.0, 2.0 );
