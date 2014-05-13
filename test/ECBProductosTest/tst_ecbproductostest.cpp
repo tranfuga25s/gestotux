fa0311b3e2d2f4ba5a66203b77400e8de73cc89c
tranfuga25s <tranfuga25s@gmail.com>
2014-05-13 17:48:53 -0300

    Agregadas prefrencias para mostrar stock en la lista de productos y test para comprobar de que salga correctamente. issue #56..

--- test/ECBProductosTest/tst_ecbproductostest.cpp
+++ test/ECBProductosTest/tst_ecbproductostest.cpp
@@ -23,6 +23,8 @@ private Q_SLOTS:
     void cleanup();
     void testFiltroProveedor();
     void testFiltroProveedor_data();
+    void testMostrarStock();
+    void testMostrarStock_data();
 
 private:
      ECBProductos *ecb;
@@ -66,6 +68,53 @@ void ECBProductosTest::testFiltroProveedor_data()
     QTest::addColumn<int>("conteo");
     QTest::newRow("Proveedor 1") << 1 << 1;
     QTest::newRow("Proveedor 2") << 2 << 1;
+}
+
+#include "preferencias.h"
+/*!
+ * \brief ECBProductosTest::testMostrarStock
+ * Permite probar la característica de mostrar el stock al lado del nombre del producto
+ */
+void ECBProductosTest::testMostrarStock()
+{
+    QFETCH( bool, habilitado );
+    QFETCH( int, posicion );
+    QFETCH( double, stock );
+
+    preferencias *p = preferencias::getInstancia();
+    p->beginGroup( "Preferencias" );
+    p->beginGroup( "Productos" );
+    p->setValue( "?", habilitado );
+    p->endGroup();
+    p->endGroup();
+    p=0;
+
+    ECBProductos *ecb = new ECBProductos();
+    QTest::qWait( 1000 );
+    if( habilitado ) {
+        QString texto = ecb->itemText( posicion );
+        QVERIFY( !texto.isEmpty() );
+        QStringList lista = texto.split( '(' );
+        QVERIFY( lista.size() > 0 );
+        QString test = lista.last();
+        test.remove( ")" );
+        QVERIFY( test.toDouble() == stock );
+    } else {
+        QString texto = ecb->itemText( posicion );
+        QVERIFY( !texto.isEmpty() );
+        QStringList lista = texto.split( '(' );
+        QVERIFY( lista.size() <= 0 );
+    }
+    delete ecb;
+}
+
+void ECBProductosTest::testMostrarStock_data()
+{
+    QTest::addColumn<bool>("habilitado");
+    QTest::addColumn<int>("posicion");
+    QTest::addColumn<double>("stock");
+    QTest::newRow("ConStock") << true << 1 << 1.0;
+    QTest::newRow("SinStock") << false << 1 << 0.0;
 }
 
 QTEST_MAIN(ECBProductosTest)
