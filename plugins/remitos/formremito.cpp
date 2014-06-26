f22cbb363cabe408aac78aab82634f5955aee0a9
tranfuga25s <tranfuga25s@gmail.com>
2014-06-26 11:34:56 -0300

    Agregado codigo para cargar los datos al formulario.

--- plugins/remitos/formremito.cpp
+++ plugins/remitos/formremito.cpp
@@ -1,5 +1,14 @@
 #include "formremito.h"
 
+#include "MRemito.h"
+#include "mitemremito.h"
+#include "NumeroComprobante.h"
+#include "mproductostotales.h"
+
+#include <QSqlField>
+#include <QSqlRecord>
+
+
 FormRemito::FormRemito(QWidget *parent) :
 EVentana(parent), FormAgregarRemitoBase()
 {
@@ -18,6 +27,9 @@ EVentana(parent), FormAgregarRemitoBase()
     GBFormaPago->setEnabled( false );
 
     PTEObservaciones->setReadOnly( true );
+
+    mpt = new MProductosTotales( TVProductos );
+    TVProductos->setModel( mpt );
 }
 
 /*!
@@ -25,6 +37,54 @@ EVentana(parent), FormAgregarRemitoBase()
  */
 void FormRemito::setearValor( const int id_remito )
 {
-    qWarning( "No implementado!" );
-    return;
+
+    MRemito *mr = new MRemito();
+    QSqlRecord r = mr->obtenerDatos( id_remito );
+    delete mr;
+    mr = 0;
+
+    DEFecha->setDate( r.field( "fecha" ).value().toDate() );
+    CBCliente->setearId( r.field("id_cliente").value().toInt() );
+    LEDireccion->setText( r.field("direccion").value().toString() );
+    PTEObservaciones->setPlainText( r.field("observaciones").value().toString() );
+    LNumeroComprobante->setText( NumeroComprobante( LNumeroComprobante,
+                                                    r.field("serie").value().toInt(),
+                                                    r.field("numero").value().toInt() ).aCadena() );
+
+
+    switch( r.field("forma_pago").value().toInt() ) {
+        case MRemito::Contado: {
+            RBContado->setChecked( true );
+            break;
+        }
+        case MRemito::CuentaCorriente: {
+            RBCtaCte->setChecked( true );
+            break;
+        }
+        case MRemito::Cuotas: {
+            RBCuotas->setChecked( true );
+            break;
+        }
+        case MRemito::Otro: {
+            RBOtro->setChecked( true );
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+
+    MItemRemito *ir = new MItemRemito();
+    ir->cargarItems( id_remito );
+    for( int i = 0; i < ir->rowCount(); i++ ) {
+        // Cargo los datos al modelo
+        mpt->agregarItem(
+                    ir->data( ir->index( i, ir->fieldIndex( "cantidad" ) )       , Qt::EditRole ).toDouble(),
+                    ir->data( ir->index( i, ir->fieldIndex( "item" ) )           , Qt::EditRole ).toString(),
+                    ir->data( ir->index( i, ir->fieldIndex( "precio_unitario" ) ), Qt::EditRole ).toDouble()
+        );
+    }
+    delete ir;
+    ir = 0;
+
 }
--- plugins/remitos/formremito.h
+++ plugins/remitos/formremito.h
@@ -4,6 +4,8 @@
 #include "eventana.h"
 #include "ui_FormAgregarRemitoBase.h"
 
+class MProductosTotales;
+
 class FormRemito : public EVentana, public Ui::FormAgregarRemitoBase
 {
     Q_OBJECT
@@ -11,6 +13,9 @@ public:
     FormRemito(QWidget *parent = 0);
 
     void setearValor( const int id_remito );
+
+private:
+    MProductosTotales *mpt;
     
 };
 
